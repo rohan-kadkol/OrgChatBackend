341ec52d6043432269d486c92f962a67308a544e
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-20 17:54:30 -0500

    Added GET /types

--- app.py
+++ app.py
@@ -49,3 +49,20 @@ def organizations():
         'success': True,
         'organizations': organizations
     })
+
+
+@app.route('/types', methods=['GET'])
+def types():
+    results = conn.execute("""  select  ID,
+                                        name 
+                                from type;""")
+    types = []                                
+    for row in results:
+        types.append({
+            'ID': row['ID'],
+            'name': row['name']
+        })
+    return jsonify({
+        'success': True,
+        'types': types
+    })
