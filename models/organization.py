05265a7afa83895404b11bd23bec56ae21ae87b8
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-22 23:23:28 -0500

    Converted old orgs to testing and new GET /organizations now does query

--- models/organization.py
+++ models/organization.py
@@ -1,8 +1,8 @@
-from flask import jsonify
+from flask import jsonify, request
 from init_flask import app, conn
 
-@app.route('/organizations', methods=['GET'])
-def organizations():
+@app.route('/test/organizations', methods=['GET'])
+def test_organizations():
     results = conn.execute(
         """ select  organization.ID, 
                     organization.name,
@@ -10,6 +10,32 @@ def organizations():
                     organization.location 
             from organization join type
             where organization.type = type.ID;""")
+    organizations = []
+    for row in results:
+        organizations.append({
+            'ID': row['ID'],
+            'name': row['name'],
+            'type': row['type'],
+            'location': row['location']
+        })
+    return jsonify({
+        'success': True,
+        'organizations': organizations
+    })
+
+@app.route('/organizations', methods=['GET'])
+def organizations():
+    query = request.args.get('query')
+
+    results = conn.execute(
+        f""" select  organization.ID, 
+                    organization.name,
+                    type.name as type,
+                    organization.location 
+            from organization join type
+            where   organization.type = type.ID and
+                    organization.name like '%%{query}%%' or
+                    organization.location like '%%{query}%%';""")
     organizations = []
     for row in results:
         organizations.append({
