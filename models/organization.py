9854095a9e6f9c23942b5454f47577872e9da34d
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-25 17:14:26 -0600

    Changed the return fields of /org/users

--- models/organization.py
+++ models/organization.py
@@ -50,23 +50,24 @@ def organizations():
     })
 
 @app.route('/organizations/<int:organization_id>/users', methods=['GET'])
+
+# organization.ID as organization_id,
+# 	                organization.name as organization_name,
+# 	                type.name as organization_type,
+# 	                organization.location as organization_location
+
 def organization_users(organization_id):
     results = conn.execute(
         f""" select 	user.ID as user_id,
 	                user.name as user_name,
 	                user.phone_number as user_phone_number,
-	                user.email as user_email,
-	                organization.ID as organization_id,
-	                organization.name as organization_name,
-	                type.name as organization_type,
-	                organization.location as organization_location
+	                user.email as user_email
 
             from 	user join registration
-	                join organization join type
+	                join organization
 
             where 	user.ID = registration.UID and 
-	                organization.id = registration.OID and
-	                organization.type = type.ID
+	                organization.id = registration.OID
                     
                     and
                     
@@ -75,14 +76,10 @@ def organization_users(organization_id):
     registrations = []
     for row in results:
         registrations.append({
-            'user_id': row['user_id'],
-            'user_name': row['user_name'],
-            'user_phone_number': row['user_phone_number'],
-            'user_email': row['user_email'],
-            'organization_id': row['organization_id'],
-            'organization_name': row['organization_id'],
-            'organization_type': row['organization_type'],
-            'organization_location': row['organization_location'],
+            'ID': row['user_id'],
+            'name': row['user_name'],
+            'phone_number': row['user_phone_number'],
+            'email': row['user_email'],
         })
     return jsonify({
         'success': True,
