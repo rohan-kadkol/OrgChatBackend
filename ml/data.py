01b56aa3a57adfdb0ab1fc96ca48295953e375a1
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-29 11:55:40 -0600

    Removed print logs from POST /prediction/data

--- ml/data.py
+++ ml/data.py
@@ -7,13 +7,7 @@ def add_data():
     data = str(request.json['data'])
     _class = int(request.json['class'])
 
-    print(data)
-    print(_class)
-
     split = data.split(',')
-
-    print(split)
-    print(len(split))
 
     if len(split) != 1600:
         return jsonify({'error': 'Must pass 1600 features where each feature can take the value of 0 or 1.'}), 400
