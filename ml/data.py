181b3392f66f9d976b3ca0dffe901bfa99b24b48
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-29 10:50:06 -0600

    POST /data now takes timestamp

--- ml/data.py
+++ ml/data.py
@@ -5,11 +5,12 @@ from init_flask import app, conn_ml
 @app.route('/prediction/data', methods=['POST'])
 def add_data():
     data = request.json['data']
+    class = request.json['class']
 
     split = data.split(',')
     if len(split) != 1600:
         return jsonify({'error': 'Must pass 1600 features where each feature can take the value of 0 or 1.'}), 400
 
-    conn_ml.execute("""insert into data (data, timestamp) values (%(data)s, now());""", {'data': data})
+    conn_ml.execute("""insert into data (data, timestamp, class) values (%(data)s, now(), %(class)d);""", {'data': data, 'class': class})
 
     return jsonify({'success': True})
