06dab8d392b678030747ab64a19d46d0d8d23beb
Ubuntu <ubuntu@ip-172-31-29-170.us-east-2.compute.internal>
2020-11-29 17:13:48 +0000

    Trying to get POST/data to work

--- ml/data.py
+++ ml/data.py
@@ -5,12 +5,19 @@ from init_flask import app, conn_ml
 @app.route('/prediction/data', methods=['POST'])
 def add_data():
     data = request.json['data']
-    class = request.json['class']
+    _class = request.json['class']
+
+    print(data)
+    print(_class)
 
     split = data.split(',')
+
+    print(split)
+    print(len(split))
+
     if len(split) != 1600:
         return jsonify({'error': 'Must pass 1600 features where each feature can take the value of 0 or 1.'}), 400
 
-    conn_ml.execute("""insert into data (data, timestamp, class) values (%(data)s, now(), %(class)d);""", {'data': data, 'class': class})
+    conn_ml.execute("""insert into data (data, timestamp, class) values (%(data)s, now(), %(class)d);""", {'data': data, 'class': _class})
 
     return jsonify({'success': True})
