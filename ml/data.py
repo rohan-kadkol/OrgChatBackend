eb4c8892d7b65479d5b1522e0748f0b6b33297c7
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-11-29 11:37:38 -0600

    Working on POST /prediction/data

--- ml/data.py
+++ ml/data.py
@@ -4,8 +4,8 @@ from init_flask import app, conn_ml
 
 @app.route('/prediction/data', methods=['POST'])
 def add_data():
-    data = request.json['data']
-    _class = request.json['class']
+    data = str(request.json['data'])
+    _class = int(request.json['class'])
 
     print(data)
     print(_class)
@@ -18,6 +18,6 @@ def add_data():
     if len(split) != 1600:
         return jsonify({'error': 'Must pass 1600 features where each feature can take the value of 0 or 1.'}), 400
 
-    conn_ml.execute("""insert into data (data, timestamp, class) values (%(data)s, now(), %(class)d);""", {'data': data, 'class': _class})
+    conn_ml.execute("""insert into data (data, timestamp, class) values (%(data)s, now(), %(class)s);""", {'data': data, 'class': int(_class)})
 
     return jsonify({'success': True})
